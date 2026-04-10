9d2d56c30f19b63ad45851a3de8a67622c3fb705
cub8 <tsubait8@gmail.com>
2026-04-10 08:48:31 +0200

    Fix test and preview for passwordless mailer.

--- test/mailers/previews/passwordless_mailer_preview.rb
+++ test/mailers/previews/passwordless_mailer_preview.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/passwordless_mailer
 class PasswordlessMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/passwordless_mailer/token_email
   def token_email
-    PasswordlessMailer.token_email
+    token_link = 'http://example.com/test'
+    email = 'jan.nowak@example.com'
+    user_name = 'Jan Nowak'
+
+    PasswordlessMailer.with(
+      token_link: token_link,
+      email:      email,
+      user_name:  user_name,
+    ).token_email
   end
 end
