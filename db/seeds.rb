0399f8cf806c38aba23675ebf0e931f8878a70a1
cub8 <tsubait8@gmail.com>
2026-04-27 12:45:10 +0200

    Add seeds for story groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 return unless Rails.env.development?
 
 def build_students
-  print('Building students')
+  puts 'Building students'
 
   students = [
     { email: 'jan.nowak@example.com', full_name: 'Jan Nowak', university_number: 's123456' },
@@ -30,11 +30,11 @@ def build_students
     )
   end
 
-  puts(' - Finished building students')
+  puts ' - Finished building students'
 end
 
 def build_teachers
-  print('Building teachers')
+  puts 'Building teachers'
 
   teachers = [
     { email: 'tomasz.nowakowski@example.com', full_name: 'Tomasz Nowakowski', university_number: 't123456' },
@@ -55,11 +55,11 @@ def build_teachers
     )
   end
 
-  puts(' - Finished building teachers')
+  puts ' - Finished building teachers'
 end
 
 def build_admins
-  print('Building admins')
+  puts 'Building admins'
 
   admins = [
     {
@@ -88,11 +88,58 @@ def build_admins
     )
   end
 
-  puts(' - Finished building admins')
+  puts ' - Finished building admins'
+end
+
+def build_story_group
+  puts 'Building story group'
+
+  owner = User.find_by(email: 'tomasz.nowakowski@example.com')
+  teachers = [
+    User.find_by(email: 'barbara.kowalczyk@example.com'),
+    User.find_by(email: 'andrzej.wozniak@example.com'),
+  ]
+  students = User.where(role: :student).first(5) + [User.where(role: :teacher).last!]
+  description = <<~DESC
+    Galaktyka jest wielka, ale nasze uszy są większe!
+    Dołącz do pionierów, którzy zamienili nory na stacje orbitalne.
+    Walczymy o prestiż, przetrwanie i pełne brzuchy, zbierając Złote Marchewki w świecie, gdzie grawitacja to tylko sugestia.
+    Pamiętaj: w kosmosie nikt nie usłyszy Twojego chrupania... chyba że zapomnisz wyłączyć interkom
+  DESC
+
+  story_group = FactoryBot.create(
+    :story_group,
+    owner:         owner,
+    name:          'Kosmiczne króliki',
+    description:   description,
+    currency_name: 'Złota Marchewka',
+    default_lives: 3,
+  )
+
+  story_group.teachers << teachers
+  story_group.students << students
+
+  icon_path = Rails.root.join('db', 'seeds', 'images', 'space-bunnies.jpg')
+  currency_icon_path = Rails.root.join('db', 'seeds', 'images', 'golden_carrot.png')
+
+  story_group.icon.attach(
+    io:           File.open(icon_path),
+    filename:     'space-bunnies.jpg',
+    content_type: 'image/jpeg',
+  )
+
+  story_group.currency_icon.attach(
+    io:           File.open(currency_icon_path),
+    filename:     'golden_carrot.png',
+    content_type: 'image/png',
+  )
+
+  puts '- Finished building story group'
 end
 
 build_students
 build_teachers
 build_admins
+build_story_group
 
 # rubocop:enable Style/TopLevelMethodDefinition
