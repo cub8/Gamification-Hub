f5b4ad05e591641082baf16ef25656aaeeb02bef
cub8 <tsubait8@gmail.com>
2026-04-27 14:39:11 +0200

    Get rid of hello controller

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -1,4 +1,3 @@
-import "@controllers/hello_controller"
 import "@controllers/sortable_form_controller"
 import "@controllers/nested_rondo_controller"
 import "@controllers/tom_select_user_controller"
