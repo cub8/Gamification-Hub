40e13694976c6a2b0fda6118bea1767fc944283b
brauzik123 <tomek.brauza@gmail.com>
2026-05-21 01:45:43 +0200

    rerouting przycisku moje przedmioty

--- app/components/shared/main_sidebar_component.rb
+++ app/components/shared/main_sidebar_component.rb
@@ -50,7 +50,11 @@ class Shared::MainSidebarComponent < ViewComponent::Base
       [
         { text: 'Sklep', path: helpers.story_group_shop_index_path(story_group), icon: 'fa-sack-dollar' },
         { text: 'Ranking', path: helpers.story_group_ranking_path(story_group), icon: 'fa-ranking-star' },
-        { text: 'Moje przedmioty', path: helpers.story_group_items_path(story_group), icon: 'fa-flask' },
+        {
+          text: 'Moje przedmioty',
+          path: helpers.story_group_student_students_items_path(story_group, user),
+          icon: 'fa-flask',
+        },
         { text: 'Rangi', path: helpers.story_group_ranks_path(story_group), icon: 'fa-angles-up' },
         { text: 'Odznaki', path: helpers.story_group_badges_path(story_group), icon: 'fa-award' },
         { text: 'Nauczyciele', path: helpers.story_group_teachers_path(story_group), icon: 'fa-briefcase' },
