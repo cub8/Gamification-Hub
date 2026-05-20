e32ad0894804fa920463ddd216c8627f41d30171
brauzik123 <tomek.brauza@gmail.com>
2026-05-21 01:46:22 +0200

    added show and index

---
+++ app/controllers/students_items_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class StudentsItemsController < ApplicationController
+  before_action :set_story_group
+  before_action :set_student
+  before_action :authorize_access!
+
+  def index
+    @students_items = @student.students_items.includes(:item).order(created_at: :desc)
+  end
+
+  def show
+    @students_item = @student.students_items.find(params[:id])
+  end
+
+  private
+
+  def set_story_group
+    @story_group = StoryGroup.find(params[:story_group_id])
+  end
+
+  def set_student
+    @student = @story_group.student_memberships.find(params[:student_id])
+  end
+
+  def authorize_access!
+    return if current_user.teacher?
+    return if @story_group.owner_id == current_user.id
+    return if current_user == @student.user
+
+    redirect_to home_path, alert: 'Brak dostępu do tej akcji.'
+  end
+end
