cbf43a567aabb117ae67765ad2f4c3136f81fb78
cub8 <tsubait8@gmail.com>
2026-05-26 15:31:12 +0200

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,6 +26,23 @@ module ActionDispatch
     def sign_out
       delete logout_path
     end
+
+    def assert_turbo_redirected_to(expected_url)
+      assert_response :success
+      assert_equal 'text/vnd.turbo-stream.html', response.media_type
+
+      if response.body =~ /turbo-stream action="redirect" target="([^"]+)"/
+        actual_url = ::Regexp.last_match(1)
+
+        expected_path = URI.parse(expected_url).path || expected_url
+        actual_path   = URI.parse(actual_url).path
+
+        assert_equal expected_path, actual_path,
+                     "Expected to be Turbo-redirected to <#{expected_path}> but was redirected to <#{actual_path}>"
+      else
+        flunk 'Expected a Turbo Stream redirect, but none was found in the response body.'
+      end
+    end
   end
 end
 
