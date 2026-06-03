c59683db0f1579893282719b2b633fa42a71e8b1
cub8 <tsubait8@gmail.com>
2026-06-03 11:45:06 +0200

    Fix fontawesome flashes - move rendering font awesome to css instead js

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,9 +1,6 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "@controllers"
 import "bootstrap"
-import "@fortawesome/fontawesome-free/js/all"
-
 import "@utils/bootstrap_setup"
 
 Turbo.StreamActions.redirect = function() {
